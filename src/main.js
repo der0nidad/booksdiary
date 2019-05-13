7ac555b48d30b7ef13bcc02a0364fe938c33ccc6
Y S <der0nidad@gmail.com>
2019-05-13 22:42:28 +0300

    Initialized project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+// YourComponent.vue or main.js for global usage
+import VueScrollProgress from 'vue-scroll-progress'
+
+Vue.use(VueScrollProgress)
 
 Vue.config.productionTip = false
 
