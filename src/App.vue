88621670c2b8f8216fe2db011fc638297fa9713d
Y S <der0nidad@gmail.com>
2019-05-14 09:06:04 +0300

    Moved toolbar to NavigationToolbar component

--- src/App.vue
+++ src/App.vue
@@ -4,30 +4,7 @@
       <VueScrollProgress></VueScrollProgress>
     </v-flex>
 
-    <v-toolbar app flat>
-      <v-spacer></v-spacer>
-
-<!--      <v-toolbar-title>Title</v-toolbar-title>-->
-          <v-menu :nudge-width="100">
-            <template v-slot:activator="{ on }">
-              <v-toolbar-title v-on="on">
-                <span>Books Diary</span>
-                <v-icon dark>arrow_drop_down</v-icon>
-              </v-toolbar-title>
-            </template>
-            <v-list>
-              <v-list-tile
-                      v-for="(item, index) in items"
-                      :key="index"
-                      @click=""
-              >
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-              </v-list-tile>
-            </v-list>
-          </v-menu>
-      <v-spacer></v-spacer>
-
-    </v-toolbar>
+    <NavigationToolbar/>
 
 
 
@@ -40,24 +17,19 @@
 <script>
 import HelloWorld from './components/HelloWorld'
 import BooksList from "./components/BooksList";
+import NavigationToolbar from "./components/NavigationToolbar";
 
 export default {
   name: 'App',
   components: {
+    NavigationToolbar,
     BooksList,
     HelloWorld
   },
   data () {
     return {
 
-      items: [
-        {
-          title: 'Books Diary'
-        },
-        {
-          title: 'Add new book'
-        }
-      ]
+
       //
     }
   }
---
+++ src/components/NavigationToolbar.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-toolbar app flat color="indigo">
+        <v-spacer></v-spacer>
+
+        <!--      <v-toolbar-title>Title</v-toolbar-title>-->
+        <v-menu :nudge-width="100">
+            <template v-slot:activator="{ on }">
+                <v-toolbar-title v-on="on">
+                    <span class="bd_title pointer"> Books Diary</span>
+                    <v-icon dark>arrow_drop_down</v-icon>
+                </v-toolbar-title>
+            </template>
+            <v-list>
+                <v-list-tile
+                    v-for="(item, index) in items"
+                    :key="index"
+                    @click=""
+                >
+                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile>
+            </v-list>
+        </v-menu>
+        <v-spacer></v-spacer>
+
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: "NavigationToolbar",
+        data() {
+            return {
+                items: [
+                    {
+                        title: 'Books Diary'
+                    },
+                    {
+                        title: 'Add new book'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+
+    .bd_title {
+        text-decoration: none;
+        color: #FFFFFF;
+    }
+
+    .pointer {
+        cursor: pointer;
+    }
+
+</style>
